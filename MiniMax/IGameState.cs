19ea0bafab45216e9e22b55670119602cfe5078d
Johnny Pan <Johnny.Pan@GMR.local>
2019-06-23 14:32:46 -0700

    first commit

---
+++ MiniMax/IGameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniMax
+{
+    public class IGameState
+    {
+        public int Value { get; }
+        public bool IsTerminal { get; }
+        public IEnumerable<IGameState> Moves { get; }
+    }
+}
---
+++ MiniMax/Minimaxer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniMax
+{
+    public class Minimaxer
+    {
+        int alpha = -1000;
+        int actual = 0;
+        int beta = 1000;
+
+        public Minimaxer()
+        {
+
+        }
+
+        public int Minimax(IGameState state, bool maxTurn)
+        {
+            if (state.IsTerminal) 
+            {
+                return state.Value;
+            }
+
+            if (maxTurn)
+            {
+                int value = int.MinValue;
+                foreach (IGameState move in state.Moves)
+                {
+                    value = Math.Max(value, Minimax(move, false));
+                }
+                return value;
+            }
+            else
+            {
+                int value = int.MaxValue;
+                foreach (IGameState move in state.Moves)
+                {
+                    value = Math.Min(value, Minimax(move, true));
+                }
+                return value;
+            }
+        }
+    }
+}
